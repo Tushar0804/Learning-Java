2ded337169b3410212223c0c95bc9089b82a99b1
Tushar Sharma <tusharsharma008@icloud.com>
2022-04-02 21:48:44 +0530

    Adding new program - Variables & DataTypes in Java

--- src/Unit_01/P2_Task02_ConstantsInJava.java
+++ src/Unit_01/P2_Task02_ConstantsInJava.java
@@ -1,37 +1,37 @@
-package Unit_01;
-
-public class P2_Task02_ConstantsInJava
-{
-    public static final double Pi = 3.14; // Using final keyword to declare constants variable
-
-    public static void main(String[] args)
-    {
-        // We can only use static members in static functions
-        System.out.println(Pi);
-        main("Calling the Program");
-
-        NewClass obj = new NewClass();
-        obj.add(1, 2);
-        System.out.println(obj.Pi);
-    }
-
-    public static void main(String args)
-    {
-        // main function overloaded
-        System.out.println(Pi);
-    }
-
-    void add(int a, int b)
-    {
-        System.out.println(a + b);
-        System.out.println(Pi);
-    }
-}
-class NewClass
-{
-    int add(int a, int b)
-    {
-        System.out.println(a + b);
-        System.out.println(P2_Task02_ConstantsInJava.Pi);
-    }
-}
+//package Unit_01;
+//
+//public class P2_Task02_ConstantsInJava
+//{
+//    public static final double Pi = 3.14; // Using final keyword to declare constants variable
+//
+//    public static void main(String[] args)
+//    {
+//        // We can only use static members in static functions
+//        System.out.println(Pi);
+//        main("Calling the Program");
+//
+//        NewClass obj = new NewClass();
+//        obj.add(1, 2);
+//        System.out.println(obj.Pi);
+//    }
+//
+//    public static void main(String args)
+//    {
+//        // main function overloaded
+//        System.out.println(Pi);
+//    }
+//
+//    void add(int a, int b)
+//    {
+//        System.out.println(a + b);
+//        System.out.println(Pi);
+//    }
+//}
+//class NewClass
+//{
+//    int add(int a, int b)
+//    {
+//        System.out.println(a + b);
+//        System.out.println(P2_Task02_ConstantsInJava.Pi);
+//    }
+//}
